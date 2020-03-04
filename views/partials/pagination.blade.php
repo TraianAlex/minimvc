8170bba3511b23dc5a9b4db22a16d720c70081da
Traian Alexandru <victor_traian@yahoo.com>
2020-03-04 18:05:36 -0500

    test

--- views/partials/pagination.blade.php
+++ views/partials/pagination.blade.php
@@ -1,16 +1,16 @@
 <nav aria-label="Page navigation example">
   <ul class="pagination">
     <li class="page-item">
-      <a class="page-link" href="{{ SITE_ROOT }}/{{ $url }}/1" aria-label="Previous">
+      <a class="page-link" href="{{ $url }}/1" aria-label="Previous">
         <span aria-hidden="true">&laquo;</span>
         <span class="sr-only">Previous</span>
       </a>
     </li>
     @for ($i = 1; $i < $total; $i++)
-        <li class="page-item"><a class="page-link" href="{{ SITE_ROOT }}/{{ $url }}/{{ $i }}">{{ $i }}</a></li>
+        <li class="page-item"><a class="page-link" href="{{ $url }}/{{ $i }}">{{ $i }}</a></li>
     @endfor
     <li class="page-item">
-      <a class="page-link" href="{{ SITE_ROOT }}/{{ $url }}/{{ $total }}" aria-label="Next">
+      <a class="page-link" href="{{ $url }}/{{ $total }}" aria-label="Next">
         <span aria-hidden="true">&raquo;</span>
         <span class="sr-only">Next</span>
       </a>
